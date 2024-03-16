d95bc61db9af0be4006888c6429bd12b0035b827
Eduardo <calle13.201@gmail.com>
2024-03-15 22:06:35 -0600

    Cambiar a uso de sqlite en settings.py

--- SisCoVaPro/config/settings.py
+++ SisCoVaPro/config/settings.py
@@ -79,11 +79,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db',
-        'USER': 'postgres',
-        'PASSWORD': 'Admin1234',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
